0e58b8f6cf2452b8bfd6b50129889d5fb559277f
Rohit Kaushik <kaushikrohit325@gmail.com>
2026-01-28 00:00:45 +0000

    First kernel

--- cuda.py
+++ cuda.py
@@ -19,13 +19,54 @@ img = io.read_image("./puppy.jpg")
 
 print(img.shape)
 
-def show_image(x, figsize=(5, 5)):
+def show_image(x, figsize=(5, 5), **kwargs):
     """Display an image tensor."""
     plt.figure(figsize=figsize)
     plt.axis('off')
     if len(x.shape) == 3: x = x.permute(1, 2, 0)  # CxHxW to HxWxC
-    plt.imshow(x.cpu())
+    plt.imshow(x.cpu(), **kwargs)
     plt.axis('off')
     plt.show()
 
-show_image(img)
+
+def run_kernel(f, times,*args):
+    for i in range(times): f(i, *args)
+
+def rgb2grey_k(i, x, out, n):
+    out[i] = 0.2989 * x[i] + 0.5870 * x[i + n] + 0.1140 * x[i + 2*n]
+
+def rgb2grey(x):
+    c, h, w = x.shape
+    n = h * w
+    x = x.flatten()
+    res = torch.empty((h * w,), device=x.device)
+    run_kernel(rgb2grey_k, h * w, x, res, n)
+    return res.view(h, w)
+
+# img_g = rgb2grey(img.cuda())
+# show_image(img_g, cmap='gray')
+
+
+##### Python block kernel version #####
+
+def blk_kernel(f, blocks, threads, *args):
+    for i in range(blocks):
+        for j in range(threads): f(i, j, threads, *args)
+
+def rgb2grey_blk_k(b, t, threads, x, out, n):
+    i = b * threads + t
+    if i < n:
+        out[i] = 0.2989 * x[i] + 0.5870 * x[i + n] + 0.1140 * x[i + 2*n]
+
+def rgb2grey_blk(x):
+    c, h, w = x.shape
+    n = h * w
+    x = x.flatten()
+    res = torch.empty((h * w,), device=x.device)
+    threads = 256
+    blocks = (n + threads - 1) // threads
+    blk_kernel(rgb2grey_blk_k, blocks, threads, x, res, n)
+    return res.view(h, w)
+
+img_g_blk = rgb2grey_blk(img)
+show_image(img_g_blk, cmap='gray')
