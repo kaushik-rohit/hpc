b166d1c20a4aac1a32b5c5e42e40437118a5bfb9
Rohit Kaushik <kaushikrohit325@gmail.com>
2026-01-30 00:33:00 +0000

    Matrix multiplication

---
+++ matrixMultiply.cu
@@ -0,0 +1,12 @@
+__global__ void MatrixMulKernel(float* A, float* B, float* C, int N) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < N && col < N) {
+        float value = 0;
+        for (int k = 0; k < N; ++k) {
+            value += A[row * N + k] * B[k * N + col];
+        }
+        C[row * N + col] = value;
+    }
+}
